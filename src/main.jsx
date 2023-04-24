b9f88f3ecd6bbd2c2035a09b1e90f98fc2f3cb9d
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-04-24 17:22:58 +0600

    daisyUI added, router created, Layout setup and Navbar using daisyUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world</div>,
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
+
   },
 ])
 
