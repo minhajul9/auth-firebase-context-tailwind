99f9b1b82bfe48518cf0944c35a9ba6489eb5c91
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-04-25 12:54:06 +0600

    onAuthChange for the user logged in or not check and show the informations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () =>{
+        logOut()
+        .then(() => {})
+        .catch(error => console.log(error))
+    }
+
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
+                {
+                    user ? <>
+                        <span>{user.email}</span>
+                        <button onClick={handleLogOut} className="btn btn-xs mx-4">Sign Out</button>
+                    </>
+                        : <button className="normal-case text-xl">Sign in first</button>
+                }
             </div>
 
         </div>
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -8,20 +8,39 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app)
 
 // eslint-disable-next-line react/prop-types
-const AuthProviders = ({children}) => {
+const AuthProviders = ({ children }) => {
 
     const [user, setUser] = useState(null);
 
-    const createUser = (email, password) =>{
+    const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signIn = (email, password)=> signInWithEmailAndPassword(auth, email, password)
+    const signIn = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () =>{
+        return signOut(auth)
+    }
+
+    // observe auth state change
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log('auth change', currentUser);
+            setUser(currentUser)
+        });
+
+        return () =>{
+            unsubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
     return (
